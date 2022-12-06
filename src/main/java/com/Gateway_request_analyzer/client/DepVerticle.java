7f635cc04d1c7d9eb582c7e9ff1e9284bf780df4
emme388 <emilberzelius@gmail.com>
2022-12-06 20:17:31 +0100

    Deploying several clients and several requests

---
+++ src/main/java/com/Gateway_request_analyzer/client/DepVerticle.java
@@ -0,0 +1,19 @@
+package com.Gateway_request_analyzer.client;
+
+import io.vertx.core.AbstractVerticle;
+
+import java.util.Random;
+
+public class DepVerticle extends AbstractVerticle {
+
+  int sendPort;
+  ClientVerticle cliVert;
+
+  @Override
+  public void start() throws Exception{
+    for(int i = 0; i < 10; i++){
+      vertx.deployVerticle(new ClientVerticle());
+    }
+  }
+
+}
